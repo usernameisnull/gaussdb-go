dc3930146ab58d2449180840a3e481b01b0ced32
MaBing <cumt_ttr@163.com>
2025-12-16 11:18:45 +0800

    fix test cases that failed due to timeout.

--- gaussdbconn/helper_test.go
+++ gaussdbconn/helper_test.go
@@ -24,8 +24,8 @@ func closeConn(t testing.TB, conn *gaussdbconn.GaussdbConn) {
 
 // Do a simple query to ensure the connection is still usable
 func ensureConnValid(t *testing.T, gaussdbConn *gaussdbconn.GaussdbConn) {
-	// in github action may timeout,so increate from 30 to 70.
-	ctx, cancel := context.WithTimeout(context.Background(), 70*time.Second)
+	// in github action may timeout,so increate from 30 to 200.
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Second)
 	result := gaussdbConn.ExecParams(ctx, "select generate_series(1,$1)", [][]byte{[]byte("3")}, nil, nil, nil).Read()
 	cancel()
 
